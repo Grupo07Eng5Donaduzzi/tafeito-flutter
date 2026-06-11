df65418083fc337c85a03901cd9784fef01dcc6b
Gabryel <gabryelpaiva123@gmail.com>
2026-06-11 00:46:45 -0300

    feat(chat): add REST data source for message history

---
+++ lib/src/features/chat/data/datasources/chat_remote_data_source.dart
@@ -0,0 +1,56 @@
+import '../../../../core/network/api_client.dart';
+import '../../domain/entities/chat_message.dart';
+import '../models/chat_message_dto.dart';
+
+abstract interface class ChatRemoteDataSource {
+  Future<List<ChatMessage>> getServiceMessages(
+    String serviceId, {
+    int page,
+    int pageSize,
+  });
+}
+
+class ApiChatRemoteDataSource implements ChatRemoteDataSource {
+  const ApiChatRemoteDataSource({required ApiClient apiClient})
+      : _apiClient = apiClient;
+
+  final ApiClient _apiClient;
+
+  @override
+  Future<List<ChatMessage>> getServiceMessages(
+    String serviceId, {
+    int page = 1,
+    int pageSize = 50,
+  }) async {
+    final response = await _apiClient.get(
+      '/v1/chat/services/$serviceId/messages',
+      queryParameters: {
+        'page': '$page',
+        'pageSize': '$pageSize',
+      },
+    );
+
+    final list = _extractList(response);
+    final messages = list
+        .whereType<Map>()
+        .map((json) => ChatMessageDto.fromJson(asJsonObject(json)))
+        .toList()
+      ..sort((a, b) => a.createdAt.compareTo(b.createdAt));
+
+    return messages;
+  }
+
+  List<Object?> _extractList(Object? response) {
+    final unwrapped = unwrapJsonData(response);
+    if (unwrapped is List) {
+      return asJsonList(unwrapped);
+    }
+    if (unwrapped is Map) {
+      final data = unwrapped['data'];
+      if (data is List) {
+        return asJsonList(data);
+      }
+    }
+    return const <Object?>[];
+  }
+}
