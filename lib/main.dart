18d5653eac64d79d337a85817e5eabc1c5e960aa
Gabryel <gabryelpaiva123@gmail.com>
2026-06-11 23:40:35 -0300

    chore: drop firebase_options and .vscode launch config

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
 import 'src/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+    await Firebase.initializeApp();
   } on Object catch (error) {
     debugPrint('Firebase nao inicializado: $error');
   }
